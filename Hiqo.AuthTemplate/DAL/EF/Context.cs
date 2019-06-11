d65751c5f9d978fbc167d1086fe9ed27339dd55f
Serafim Lazuka <serafim.lazuko@gmail.com>
2019-06-11 15:18:46 +0300

    create project. add DAL(engeneering in progress)

---
+++ Hiqo.AuthTemplate/DAL/EF/Context.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using DAL.Entities;
+
+namespace DAL.EF
+{
+    public class Context : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+        static Context()
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+
+    
+}
---
+++ Hiqo.AuthTemplate/DAL/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DAL.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public int Role_id { get; set; }
+    }
+}
